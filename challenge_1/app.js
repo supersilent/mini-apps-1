bd6c31aac2c45e1990353075e2cb55926ebb17a9
supersilent <shrikethrush@gmail.com>
2018-12-10 14:43:14 -0800

    Complete bare requirement.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,22 +1,57 @@
 let continueFlag = true;
-let currentChar = "X";
+let currentChar = "x";
 let obj = {
-  X: "O",
-  O: "X"
+  x: "o",
+  o: "x"
 };
 let detectWin = () => {
-  for (let i of ["X", "O"]) {
+  for (let i of ["x", "o"]) {
     for (let k = 0; k < 3; k++) {
       for (let j = 0 + k * 3; j < 3 + k * 3; j++) {
         if (document.getElementById(`${j}`).innerHTML !== i) {
           break;
         }
         if (j === 2 + k * 3) {
+          document.getElementById("result").innerHTML = `${i} is Winner!`;
           return true;
         }
       }
     }
+
+    for (let m = 0; m < 3; m++) {
+      for (let l = 0 + m; l < 9; l += 3) {
+        if (document.getElementById(`${l}`).innerHTML !== i) {
+          break;
+        }
+        if (l === 6 + m) {
+          document.getElementById("result").innerHTML = `${i} is Winner!`;
+          return true;
+        }
+      }
+    }
+
+    for (let m = 0; m < 9; m += 4) {
+      if (document.getElementById(`${m}`).innerHTML !== i) {
+        break;
+      }
+
+      if (m === 8) {
+        document.getElementById("result").innerHTML = `${i} is Winner!`;
+        return true;
+      }
+    }
+    for (let m = 2; m < 9; m += 2) {
+      if (document.getElementById(`${m}`).innerHTML !== i) {
+        break;
+      }
+
+      if (m === 6) {
+        document.getElementById("result").innerHTML = `${i} is Winner!`;
+        return true;
+      }
+    }
   }
+
   return false;
 };
 
@@ -27,13 +62,22 @@ for (let i = 0; i < 9; i++) {
         document.getElementById(`${i}`).innerHTML = currentChar;
         currentChar = obj[currentChar];
         if (detectWin()) {
-          console.log("Game End");
           continueFlag = false;
         }
       }
     }
   };
 }
+
+document.getElementById('reset').onclick = () => {
+    continueFlag = true;
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(`${i}`).innerHTML = '';
+    }
+    document.getElementById("result").innerHTML = '';
+
+}
+
 
 // document.getElementById("0").onclick = () => {
 //   if (!document.getElementById("0").innerHTML) {
