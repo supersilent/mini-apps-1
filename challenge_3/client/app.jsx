d73282bc3a0bfb14e8adfdfbbc6ce0e36f09254c
supersilent <shrikethrush@gmail.com>
2018-12-12 20:46:34 -0800

    Temporal commit.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -5,10 +5,20 @@ class CheckOut extends React.Component {
 }
 
 class F1 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <form>
-        Name : <input type="text" name="name" /> <br />
+        Name :{" "}
+        <input
+          type="text"
+          name="name"
+          value={this.props.name}
+          onChange={() => this.props.func()}
+        />{" "}
+        <br />
         E-mail : <input type="text" name="e-mail" /> <br />
         Password <input type="text" name="password" /> <br />
         <input type="submit" value="Next" />
@@ -33,28 +43,51 @@ class Shopping extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPage: null,
-      name: null,
-      email: null,
-      password: null
+      currentPage: "",
+      name: "",
+      email: "",
+      password: ""
     };
+    this.handleSubmitF1 = this.handleSubmitF1.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+
   }
   handleTransition(page) {
     this.setState({ currentPage: page });
     // console.log(this.state.currentPage);
   }
-  handleSubmitF1(name, email, password) {
-    this.setState({ currentPage: "F2", name, email, password });
+  handleSubmitF1(event) {
+    const name = target.name;
+    const target = event.target;
+    this.setState({ [name]: value});
+    // this.setState({
+    //     currentPage: "F1",
+    //     name: event.target.value,
+    //     email: "",
+    //     password: ""
+    //   });
+
+    console.log(this.state);
+  }
+  handleInputChange(event) {
+
   }
 
   render() {
     let page;
+
     if (this.state.currentPage === "F1") {
       page = (
-        <F1
-          func={page => this.handleTransition(page)}
-          onSubmit={this.handleSubmitF1}
-        />
+        <form onSubumit={this.handleSubmitF1}>
+          Name :
+          <input
+            type="text" name="name" value={this.state.name} onChange={this.handleInputChange}
+          />
+          <br />
+          E-mail : <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} /> <br />
+          Password <input type="text" name="password" /> <br />
+          <input type="submit" value="Next" />
+        </form>
       );
     } else if (this.state.currentPage === "F2") {
       page = <F2 />;
