27b4592c0e7f4f5493b8f9708746d19fc5b3151f
supersilent <shrikethrush@gmail.com>
2018-12-12 16:41:49 -0800

    First commit.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,47 @@
+class CheckOut extends React.Component {
+    render() {
+        return <button onClick={() => this.props.func()}>Checkout</button>;
+    }
+}
 
+class F1 extends React.Component {
+    render(){
+        return (<form>
+            Name : <input type="text" name="name" /> <br />
+            E-mail : <input type="text" name="e-mail" /> <br />
+            Password <input type="text" name="password" /> <br />
+            <input type="submit" value="Next" />
+            </form>);
+    }
 
+}
+
+class Shopping extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            checkout: false
+        }}
+    handleCheckout(){
+        this.setState({checkout: true})
+    }
+    render(){
+        // let page;
+        console.log(this.state.checkout)
+        let page;
+        if (this.state.checkout){
+            page = <F1 />;
+        }else{
+            page = <CheckOut func={() => this.handleCheckout()} />;
+        }
+        return (page);
+        
+        
+    }
+
+}
+
+ReactDOM.render(
+    <Shopping />,
+    document.getElementById('root')
+);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+let express = require('express');
+let app = express();
+let path = require('path')
 
+app.use(express.static('public'))
 
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.listen(3000,()=> console.log('listening port 3000'))
